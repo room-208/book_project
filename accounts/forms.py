a8b4b24bfcfc4a22cf9e06f87a6b00e6786bd7fc
room-208 <yumalegomindstorm@gmail.com>
2022-09-29 19:01:37 +0900

    commit

---
+++ accounts/forms.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from symbol import import_name
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
